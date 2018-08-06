7b32a4afd696dd2cf4c8bef468a607fbcc77ce1c
Fidel Herrera <fidel_herrera128@hotmail.com>
2018-08-06 10:44:40 -0430

    primer comit

---
+++ numbers.py
@@ -0,0 +1,20 @@
+number_one = 10
+number_two = 4
+
+result = number_one + number_two
+print ("suma ", result)
+
+result = number_one - number_two
+print ("resta ", result)
+
+result = number_one * number_two
+print ("multiplicación ", result)
+
+result = number_one / number_two #regresa un numero flotante
+print ("división ", result)
+
+result = number_one // number_two #regresa un numero entero
+print ("división ", result)
+
+result = number_one ** number_two #regresa exponencial de un numero
+print ("exponencial ", result)
---
+++ variables.py
@@ -0,0 +1,2 @@
+curso_codigo_facilito = "El curso es Python 3"
+print(curso_codigo_facilito)
